73883d76e412bc508812c572a5d4e2d524c17682
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-03-08 19:10:42 +0900

    20230308

--- day7/js/index2.js
+++ day7/js/index2.js
@@ -73,7 +73,87 @@ let letters = [
     ['진', '안', '리', '이'],
     ['가', '수', '림', '나'],
     ['아', '으', '차', '운'],
-  ];
+];
 
-console.log(letters[3][0]+letters[1][3]+letters[0][1]+letters[0][3]+letters[2][2])
+console.log(letters[3][0] + letters[1][3] + letters[0][1] + letters[0][3] + letters[2][2])
 
+// 3차월 배열
+let nums = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+
+console.log(nums[1][0][1])
+
+// object
+let cat = {
+    name: '나비',
+    age: 1,
+    isCute: true,
+    mew: function () {
+        return '냐옹';
+    },
+}
+
+let introduce = {
+    name: '강민혁',
+    age: 24,
+    local: 'Daegu'
+}
+
+console.log(cat);
+console.log(cat.name);
+console.log(cat.age);
+console.log(cat.mew());
+console.log(cat['name'])
+
+console.log(`안녕하십니까 저는 ${introduce.name}입니다. 저는 올해 ${introduce.age}살 이고 사는곳은 ${introduce.local}입니다.`)
+
+// typeof
+console.log(typeof '문자');
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof {});
+console.log(typeof cat);
+console.log(typeof []);
+console.log(typeof nums);
+console.log(typeof NaN);
+console.log(typeof undefined);
+
+// 실습
+console.log(`"${typeof NaN}" isn't "${typeof 'b'}" data type`);
+console.log(`typeof 를 boolean 이나 null 에 사용하면, “${typeof cat}” 결과를 얻을 수 있습니다.`)
+
+// 형 변환
+// let mathScore = prompt("수학점수를 입력하세요");
+// let engScore = prompt("영어점수를 입력하세요");
+
+// let avg = (mathScore + engScore) / 2;
+
+// console.log(avg);
+
+let str1 = true;
+let str2 = 123;
+let str3 = null;
+
+console.log(typeof String(str1));
+console.log(typeof String(str2));
+console.log(typeof String(str3));
+
+console.log(typeof str1);
+console.log(typeof str1.toString());
+
+let n1 = true;
+let n2 = false;
+let n3 = '123.9';
+
+console.log(typeof n1);
+console.log(typeof n2);
+console.log(typeof n3);
+
+console.log(typeof Number(n1)); // 1
+console.log(typeof Number(n2)); // 0
+console.log(typeof Number(n3)); // 123.9
+console.log(parseInt(n3));
+
+let mathScore = '77';
+let engScore = '88';
+
+console.log((parseInt(mathScore) + parseInt(engScore)) / 2)
---
+++ day7/js/index3.js
@@ -0,0 +1,40 @@
+var name = '홍길동';
+console.log(name);
+
+// 재선언
+var name = '이몽룡';
+console.log(name);
+
+
+// let
+// 재선언 불가
+// 재할당 가능
+// 초기화 안해도됨
+let a = 1;
+a = 2;
+console.log(a);
+
+let aa;
+aa = 10;
+console.log(aa);
+
+// const
+// 재선언 불가
+// 재할당 불가
+// 선언과 동시에 초기화
+
+const b = 3;
+console.log(b);
+
+let q1 = 2;
+q1 = q1 - 2;
+console.log(q1);
+
+const q2 = 5;
+// q2 = q2 - 5;
+
+let $ = 5;
+console.log($);
+
+let _ = 1;
+console.log(_);
