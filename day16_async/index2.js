114036f5f31d2a22b9fdc0c7146e7176624cacfd
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-03-27 19:04:27 +0900

    20230327

--- day16_async/index2.js
+++ day16_async/index2.js
@@ -107,3 +107,53 @@ add(4, 3, function (x) {
     })
 })
 */
+
+// promise 체이닝 이용
+// 장점1. then 메서드 연속 사용 가능 ! -> 순차적인 작업 가능
+// 장점2. 에러처리가 쉽고 간단
+// -> catch 구문에서 한번에 에러처리 가능
+
+function add(n1, n2) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            let result = n1 + n2;
+            resolve(result)
+        }, 1000)
+    })
+}
+
+function mul(n) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            let result = n * 2;
+            resolve(result)
+        }, 700)
+    })
+}
+
+function sub(n) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            let result = n - 1;
+            if (result > 20) {
+                console.log(result + " great!!!")
+            } else {
+                reject(new Error("걍 에러 띄워봄"));
+            }
+        }, 500)
+    })
+}
+
+// promise 체이닝
+add(4, 100)
+    .then(function (result) {
+        console.log(result)
+        return mul(result)
+    })
+    .then(function (result) {
+        console.log(result)
+        return sub(result)
+    })
+    .then(function (result) {
+        console.log(result)
+    })
