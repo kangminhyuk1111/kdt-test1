b8e43f0a2adfe329d8f0cd2104ba48c2831cc890
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-03-28 19:56:22 +0900

    node push

--- day17_node/app.js
+++ day17_node/app.js
@@ -3,18 +3,31 @@ const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs'); // view engine 등록
-app.set('/views', express.static(__dirname + '/views'));
-
+app.use('/views', express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extends: true })) // post요청으로 들어오는 모든 형식의 데이터를 파싱
+app.use(express.json()); // json 형태로 데이터를 주고 받음
 // 라우팅(routing) : 경로 설정
 // 브라우저에서 어떤 url로 접속했을 때 어떤 페이지를 보여줄 것인가 !
 
 // localhost:PORT/ 에 접속했을때, index.ejs 를 보여주겠다.
 app.get('/', (req, res) => {
     // views의 index.ejs를 찾아서 응답
-    res.render('index');
+    const myTitle = '폼 실습을 합시다 !!';
+    res.render('index', { title: myTitle });
+})
+
+app.get('/getForm', (req, res) => {
+    console.log(req.query);
+    res.send('get요청 성공 !');
+})
+
+app.post('/postForm', (req, res) => {
+    console.log(req.body);
+    res.send('POST요청 성공')
 })
 
 app.listen(PORT, () => {
-    console.log(`SERVER RUN IN http://localhost:${PORT}`)
+    console.log(`SERVER RUN IN http://localhost:${PORT}`);
 })
+
 
