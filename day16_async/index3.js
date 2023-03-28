1c50880ccd336668ea06ceeeb333da4fd0f00524
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-03-28 17:58:13 +0900

    20230328-3

--- day16_async/index3.js
+++ day16_async/index3.js
@@ -129,3 +129,60 @@ changeBgColor('red')
     })
 
     */
+
+// 실습2. Promise -> async/await 기본코드
+// call, back, hell 함수: 실습 23의 함수와 동일
+/*
+function call(name) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log(name);
+            resolve(name); // 작업 성공시 then(name)
+        }, 1000);
+    });
+}
+
+function back(back) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('back');
+            resolve('back'); // 작업 성공시 then('back')
+        }, 1000);
+    });
+}
+
+function hell() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve('callback hell');
+        }, 1000);
+    });
+}
+async function exec() {
+    let user = await call("kim");
+    console.log(`${user} 반가워`);
+    let txt = await back();
+    console.log(`${txt} 를 실행했구나`);
+    let message = await hell();
+    console.log("여기는 " + message);
+}
+exec(); */
+
+const changeBgColor = (newColor) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = newColor;
+            res();
+        }, 1000);
+    })
+}
+
+async function exec() {
+    await changeBgColor('red');
+    await changeBgColor('yellow');
+    await changeBgColor('green');
+    await changeBgColor('blue');
+    await changeBgColor('orange');
+}
+
+exec();
