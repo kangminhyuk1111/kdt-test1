a4ddffbd86255ed4ec3ff935f3ef81c63ef3f58b
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-04-01 11:31:50 +0900

    20230401

--- day19_file/app.js
+++ day19_file/app.js
@@ -3,10 +3,11 @@ const app = express();
 const PORT = 8000;
 // multer 미들웨어 사용하기
 const multer = require('multer'); // multer 불러오기
+const { userInfo } = require('os');
 const path = require('path'); // path 불러오기 (내장 모듈) => 파일, 폴더 경로를 쉽게 설정
-// const upload = multer({
-//     dest: 'uploads/', // dest: 업로드할 파일 경로 지정
-// });
+const upload = multer({
+    dest: 'uploads/', // dest: 업로드할 파일 경로 지정
+});
 const uploadDetail = multer({
     storage: multer.diskStorage({
         destination(req, file, done) {
@@ -14,7 +15,7 @@ const uploadDetail = multer({
         },
         filename(req, file, done) {
             const ext = path.extname(file.originalname); // file.originalname에서 확장자 추출
-            done(null, path.basename(file.originalname, ext) + Date.now() + ext)
+            done(null, req.body.id + ext)
             // [파일명 + 현재시간.확장자] 형식으로 파일 업로드
         }
     }),
@@ -46,6 +47,32 @@ app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
     // { title: '이것은 망고다!!!' } // input title 에 입력합 값
     res.send('upload 완료~!!');
 });
+
+app.post('/upload/array', uploadDetail.array('userfile'), (req, res) => {
+    console.log(req.files)
+    console.log(req.body)
+    res.send('files upload finish')
+})
+
+app.post('/upload/fields', uploadDetail.fields([{ name: 'userfile1' }, { name: 'userfile2' }]), (req, res) => {
+    console.log(req.files)
+    console.log(req.body)
+    res.send('files upload finish')
+}
+)
+
+app.post('/upload/userData', uploadDetail.single('userfile'), (req, res) => {
+    console.log(req.file)
+    console.log(req.body)
+    res.render('result', {
+        userId: req.body.id,
+        userPw: req.body.pw,
+        userName: req.body.userName,
+        userAge: req.body.age,
+        userImg: req.file.filename,
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
