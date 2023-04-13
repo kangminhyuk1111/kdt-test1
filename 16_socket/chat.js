d86c881c25add4d9924160b0be2ee2e1a3fa9af0
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-04-13 21:45:21 +0900

    message

--- 16_socket/chat.js
+++ 16_socket/chat.js
@@ -1,17 +1,77 @@
 const express = require('express');
 const app = express();
-const PORT = 6000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = 8000;
 
 app.set('view engine', 'ejs');
-app.use('/views', express.static(__dirname + '/views'))
-app.use('/static', express.static(__dirname + '/static'))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use('/views', express.static(__dirname + '/views'));
+app.use('/static', express.static(__dirname + '/static'));
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.render('chat');
-})
+});
 
-app.listen(PORT, () => {
-    console.log(`run ${PORT}`);
-})
+const nickObj = {
+
+};
+// io.on(event_name, callback)
+// : socket과 관련된 통신 작업 처리
+const updateNickList = () => {
+    io.emit('updateNicks', nickObj);
+}
+
+io.on('connection', (socket) => {
+    // "connection" event
+    // - 클라이언트가 접속했을 때 발생하는 이벤트
+    // - 콜백으로 socket 객체를 제공
+
+    // 최초 입장시 (연걸)
+    // socket.id: 소켓 고유아이디 -> socket은 웹 페이지별로 id생성
+    // -> 크롬에서 탭2개 띄우면 socket.id는 각각 생김 (2개)
+    socket.emit('userCount', io.engine.clientsCount)
+
+    console.log('⭕ Server Socket Connected >> ', socket.id);
+
+    // 채팅창 입장 안내
+
+    socket.on('setNick', (nickname) => {
+        if (Object.values(nickObj).indexOf(nickname) > -1) {
+            socket.emit("error", '닉네임이 존재합니다.')
+        } else {
+            nickObj[socket.id] = nickname;
+            io.emit('userComein', `${nickname}님이 입장하셨습니다.`);
+            socket.emit('entrySuccess', nickname);
+            updateNickList();
+            console.log(nickObj);
+        }
+    })
+
+    socket.on('sendMsg', (msg) => {
+        const sendObj = {
+            who: nickObj[socket.id],
+            msg: msg[0],
+            user: nickObj[msg[1]],
+        }
+        console.log(msg)
+        if (msg[1] == 'all') {
+            io.emit('getMsg', { who: nickObj[socket.id], msg: msg[0] })
+        } else {
+            io.to(socket.id).emit('secretMsg', sendObj)
+            io.to(msg[1]).emit('secretMsg', sendObj)
+        }
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + " is out !")
+        io.emit('userOut', nickObj[socket.id])
+        delete nickObj[`${socket.id}`];
+        updateNickList();
+    })
+});
+
+// 주의) socket 을 사용할 때는 http.listen으로 PORT 열어야 함!!!
+http.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
---
+++ 16_socket/static/js/index.js
@@ -0,0 +1,150 @@
+// frontend js
+
+// socket 사용을 위해서 객체 생성
+let socket = io.connect();
+let myNick;
+const chatList = document.querySelector('#chat-list');
+const chatBox = document.querySelector(".chat-box");
+
+socket.on('connect', () => {
+    console.log('⭕️ Client Socket Connected >> ', socket.id);
+});
+
+socket.on('userCount', (count) => {
+    console.log(count)
+    document.querySelector('#user-count').textContent = `${count}명 접속중 !`
+})
+
+socket.on('userComein', (data) => {
+    console.log(data)
+    chatList.insertAdjacentHTML('beforeend', `<p id="user-count">${data}</p>`);
+})
+
+socket.on('entrySuccess', (nickname) => {
+    myNick = nickname;
+    document.querySelector('.chat-box').classList.remove('d-none');
+    document.querySelector('#nickname').disabled = true;
+    document.querySelector('.entry-box > button').disabled = true;
+})
+
+socket.on('updateNicks', (obj) => {
+    let nickList = document.querySelector('#nick-list');
+    let options = `<option value="all">전체</option>`;
+    for (let key in obj) {
+        options += `<option value="${key}">${obj[key]}</option>`;
+    }
+    document.querySelector('#nick-list').innerHTML = options;
+})
+
+socket.on('getMsg', (data) => {
+    chatList.scrollTop = chatList.scrollHeight
+    if (myNick == data.who) {
+        const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        div1.setAttribute("class", 'my-chat');
+        div2.innerHTML = `${data.who} : ${data.msg}`
+        div1.append(div2)
+        chatList.append(div1);
+    } else if (myNick != data.who) {
+        const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        div1.setAttribute("class", 'other-chat');
+        div2.innerHTML = `${data.who} : ${data.msg}`
+        div1.append(div2)
+        chatList.append(div1);
+    }
+
+})
+
+socket.on('secretMsg', (data) => {
+    chatList.scrollTop = chatList.scrollHeight
+    if (myNick == data.who) {
+        const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        div1.setAttribute("class", 'my-chat');
+        div2.innerHTML = `${data.user}에게 : ${data.msg}`
+        div2.setAttribute("id", "bg-purple");
+        div1.append(div2)
+        chatList.append(div1);
+    } else {
+        const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+        div1.setAttribute("class", 'other-chat');
+        div2.innerHTML = `(귓속말) ${data.who} : ${data.msg}`
+        div2.setAttribute("id", 'bg-purple');
+        div1.append(div2)
+        chatList.append(div1);
+    }
+
+})
+
+socket.on('error', (data) => {
+    alert('닉네임 에러 !')
+})
+
+socket.on('userOut', (data) => {
+    if (data == undefined) {
+        return;
+    }
+    const p = document.createElement('p');
+    p.setAttribute('id', 'user-count');
+    p.innerHTML = `${data}님이 퇴장하였습니다.`;
+    chatList.append(p);
+})
+
+function entry() {
+    socket.emit('setNick', document.querySelector('#nickname').value);
+}
+
+function sendMsg() {
+    console.log(document.querySelector('#nick-list').value)
+    console.log(document.querySelector('#message').value)
+    socket.emit('sendMsg', ([document.querySelector('#message').value, document.querySelector('#nick-list').value]));
+}
+
+// function sayHello() {
+//     // client -> server 정보 보내기
+//     // socket.emit(event, data): 데이터 "전송"
+//     // => event 라는 이름으로 data를 전송
+//     socket.emit('hello', {
+//         who: 'client',
+//         msg: 'hello',
+//     })
+
+//     socket.on('helloKR', (data) => {
+//         const div1 = document.createElement('div');
+//         const div2 = document.createElement('div');
+//         div1.setAttribute("class", 'my-chat');
+//         div2.innerHTML = `${data.who} : ${data.msg}`
+//         div1.append(div2)
+//         chatList.append(div1);
+//     })
+
+
+//     // socket.on(event, callback) : 데이터 "받음"
+//     // event에 대해서 정보를 받아 callback함수 실행
+// }
+
+// function sayStudy() {
+//     socket.emit('study', {
+//         who: 'student',
+//         msg: 'study !!'
+//     })
+// }
+
+// function sayBye() {
+//     socket.emit('bye', {
+//         who: 'kim',
+//         msg: 'bye..',
+//     })
+
+//     socket.on('otherUser', (data) => {
+//         const chatList = document.querySelector('#chat-list');
+//         const div1 = document.createElement('div');
+//         const div2 = document.createElement('div');
+//         div1.setAttribute("class", 'other-chat');
+//         div2.innerHTML = `${data.who} : ${data.msg}`
+//         div1.append(div2)
+//         chatList.append(div1);
+//     })
+// }
