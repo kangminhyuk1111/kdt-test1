5f9342a5386331a4554ac4fbf368d37a862270a3
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-04-18 18:24:31 +0900

    20230418

---
+++ 17_react_distruct/my-app/src/ClassComponent.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export default class ClassComponent extends Component {
+    // 클래스형 컴포넌트 render() 함수 필수
+    render() {
+        return (
+            <div>
+                <h1>hello Component, Class</h1>
+            </div>
+        )
+    }
+};
---
+++ 17_react_distruct/my-app/src/FunctionComponent.js
@@ -0,0 +1,7 @@
+export default function FunctionComponent() {
+    return (
+        <div>
+            <h1>hello FunctionComponent!, Function</h1>
+        </div>
+    )
+}
